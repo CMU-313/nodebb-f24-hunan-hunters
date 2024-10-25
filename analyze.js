b0741f3746fccd31b3d09199d8228c8c70d2e73c
Tin Chi Lo <tin.tclo@gmail.com>
2024-10-24 21:05:03 -0400

    trying again

--- analyze.js
+++ analyze.js
@@ -1,11 +1,11 @@
 'use strict';
 
-// const Iroh = require('iroh');
+const Iroh = require('iroh');
 // const Iroh = require('./node_modules/iroh');
 const fs = require('fs');
 const path = require('path');
 const vm = require('vm');
-const Iroh = require('./node_modules/iroh');
+// const Iroh = require('./node_modules/iroh');
 
 
 console.log('Iroh is starting the analysis...');
